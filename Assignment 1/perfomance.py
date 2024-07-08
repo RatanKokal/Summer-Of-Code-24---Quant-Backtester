c88297d3837e7e7da21507be16a153e90530a62b
Ratan <ratanskokal@gmail.com>
2024-07-08 17:15:23 +0530

    Added web files

--- Assignment 1/perfomance.py
+++
@@ -1,9 +0,0 @@
-from matplotlib import pyplot as plt
-
-def plot_performance(df, symbol, start_date, end_date, timeframe):
-    plt.figure(figsize=(10, 7))
-    df['Close'].plot(title=f'{symbol} Performance from {start_date} to {end_date} with interval {timeframe}')
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.savefig(f'{symbol}_performance.png')
-    # plt.show()
